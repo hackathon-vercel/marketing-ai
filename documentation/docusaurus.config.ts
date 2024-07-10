977674f2c781f9c8c0cbc2fd7f2e12eb764a05fd
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-09 23:08:58 -0400

    docs: config name baseUrl

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -11,14 +11,14 @@ const config: Config = {
   url: 'https://carlosargelio.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/hackathon-vercel-2024/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'carlosargelio', // Usually your GitHub org/user name.
   projectName: 'carlosargelio.github.io', // Usually your repo name.
   trailingSlash: false,
-  
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
