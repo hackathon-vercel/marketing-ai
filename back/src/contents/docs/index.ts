a6feba627d78fed6b3e77bc64abb986905e47255
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-15 14:40:44 -0400

    fix: comment error path

--- back/src/contents/docs/index.ts
+++ back/src/contents/docs/index.ts
@@ -1,20 +1,20 @@
-import * as fs from 'node:fs';
-import * as path from 'node:path';
+// import * as fs from 'node:fs';
+// import * as path from 'node:path';
 
 
 
-export function readHtmlFileSync(): string {
-    try {
-        // Resuelve la ruta completa del archivo
-        const fullPath = path.resolve('/home/carlosargelio/CarlosArgelioGitHub/hackathon-vercel-2024/back/src/contents/docs/CreateContent.html');
-        // Lee el archivo de forma sincrónica
-        const data = fs.readFileSync(fullPath, { encoding: 'utf-8' });
-        return data;
-    } catch (err) {
-        console.error('Error leyendo el archivo:', err);
-        throw err;
-    }
-}
+// export function readHtmlFileSync(): string {
+//     try {
+//         // Resuelve la ruta completa del archivo
+//         const fullPath = path.resolve('/home/carlosargelio/CarlosArgelioGitHub/hackathon-vercel-2024/back/src/contents/docs/CreateContent.html');
+//         // Lee el archivo de forma sincrónica
+//         const data = fs.readFileSync(fullPath, { encoding: 'utf-8' });
+//         return data;
+//     } catch (err) {
+//         console.error('Error leyendo el archivo:', err);
+//         throw err;
+//     }
+// }
 
-// Ejemplo de uso
-export const htmlContent = readHtmlFileSync();
+// // Ejemplo de uso
+// export const htmlContent = readHtmlFileSync();
