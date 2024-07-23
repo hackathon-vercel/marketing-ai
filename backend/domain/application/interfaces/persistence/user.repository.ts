0fe73887279fa84fe05f3c984291cbc8b6a4c779
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-23 14:12:43 -0400

    feat(backend): infraestructure postgres

---
+++ backend/domain/application/interfaces/persistence/user.repository.ts
@@ -0,0 +1,9 @@
+import { IUser } from './../IUsers';
+
+export interface UserRepository {
+  find(id: IUser['id']): IUser;
+  findAll(): IUser[];
+  create(user: IUser): void;
+  update(user: IUser): IUser;
+  remove(id: IUser['id']): void;
+}
