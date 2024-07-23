843bc66499beb1ba792eb2a16b24f989b5ca2e1b
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-07-22 22:30:47 -0400

    feat(backend): add dto

---
+++ backend/core/dto/content.dto.ts
@@ -0,0 +1,43 @@
+import { IsArray, IsNotEmpty, IsString, IsUUID } from 'class-validator';
+
+export class ContentDto {
+  @IsUUID('4')
+  readonly id: string;
+
+  @IsString()
+  readonly title: string;
+
+  @IsString()
+  readonly description: string;
+
+  @IsString()
+  readonly objective: string;
+
+  @IsString()
+  readonly tone: string;
+
+  @IsArray()
+  readonly keyword: Array<string>;
+
+  @IsString()
+  readonly message: string;
+}
+export class CreateContentDto {
+  @IsString()
+  readonly objective: string;
+
+  @IsString()
+  readonly tone: string;
+
+  @IsArray()
+  readonly keyword: Array<string>;
+
+  @IsString()
+  readonly message: string;
+}
+
+export class KeywordsDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly term: string;
+}
