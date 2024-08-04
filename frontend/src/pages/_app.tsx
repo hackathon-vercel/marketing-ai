495f0de0a5340e203c533b93d7c3984289515588
Jisbel Rodríguez <114784056+Jisbel@users.noreply.github.com>
2024-08-04 17:19:43 -0400

    Revert "Latest screens"

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,15 +1,6 @@
-import * as React from 'react';
-import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../theme/theme';
-import '@fontsource/poppins'; 
-import '@fontsource/montserrat';
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
